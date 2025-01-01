22032c857c694c87c76a437cb7bc54a0ad6df289
Benjamin <Benjaminkakai001@gmail.com>
2025-01-01 21:40:55 +0300

    Initial commit: Customized banking system from Mifos

--- src/environments/.env.ts
+++ src/environments/.env.ts
@@ -2,8 +2,8 @@
 /* tslint:disable */
 export default {
   'mifos_x': {
-    'version': '241204',
-    'hash': '29cda98b'
+    'version': '250101',
+    'hash': '54ac7b46'
   },
   'allow_switching_backend_instance': true
 };
