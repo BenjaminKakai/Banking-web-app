3ab61a07cabdad201482600d1a0c62a8870ed156
Benjamin <Benjaminkakai001@gmail.com>
2025-01-13 08:23:01 +0300

    chore: setup nginx configuration and GitHub Actions runner for frontend deployment

--- src/environments/.env.ts
+++ src/environments/.env.ts
@@ -2,8 +2,8 @@
 /* tslint:disable */
 export default {
   'mifos_x': {
-    'version': '241204',
-    'hash': '29cda98b'
+    'version': '241220',
+    'hash': 'ba3aef49'
   },
   'allow_switching_backend_instance': true
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,22 +6,23 @@ export const environment = {
   version: env.mifos_x.version,
   hash: env.mifos_x.hash,
   // For connecting to server running elsewhere update the tenant identifier
-  fineractPlatformTenantId: window['env']['fineractPlatformTenantId'] || 'default',
-  fineractPlatformTenantIds: window['env']['fineractPlatformTenantIds'] || 'default',
-  // For connecting to others servers running elsewhere update the base API URL
-  baseApiUrls: window['env']['fineractApiUrls'] ||
+fineractPlatformTenantId: window['env']['fineractPlatformTenantId'] || 'default',
+fineractPlatformTenantIds: window['env']['fineractPlatformTenantIds'] || 'default',
+
+// For connecting to other servers running elsewhere update the base API URL
+baseApiUrls: window['env']['fineractApiUrls'] || 
   'https://dev.mifos.io,https://demo.mifos.io,https://qa.mifos.io,https://staging.mifos.io,https://mobile.mifos.io,https://demo.fineract.dev,https://localhost:8443',
+
   // For connecting to server running elsewhere set the base API URL
-  baseApiUrl: window['env']['fineractApiUrl'] || window.location.protocol + '//' + window.location.hostname,
-  oauthServerUrl: window['env']['oauthServerUrl'] || window['env']['fineractApiUrl'] + window['env']['apiProvider'],
+  baseApiUrl: 'http://167.172.152.104:8443',
+  
   allowServerSwitch: env.allow_switching_backend_instance,
   apiProvider: window['env']['apiProvider'] || '/fineract-provider/api',
   apiVersion: window['env']['apiVersion'] || '/v1',
   serverUrl: '',
   oauth: {
-    enabled: window['env']['oauthServerEnabled'] || false,  // For connecting to Mifos X using OAuth2 Authentication change the value to true
-    serverUrl: window['env']['oauthServerUrl'] || '',
-    appId: window['env']['oauthAppId'] || ''
+    enabled: false,  // For connecting to Mifos X using OAuth2 Authentication change the value to true
+    serverUrl: ''
   },
   warningDialog: {
     title: 'Warning',
@@ -49,4 +50,4 @@ export const environment = {
 
 // Server URL
 environment.serverUrl = `${environment.baseApiUrl}${environment.apiProvider}${environment.apiVersion}`;
-
+environment.oauth.serverUrl = `${environment.baseApiUrl}${environment.apiProvider}`;
